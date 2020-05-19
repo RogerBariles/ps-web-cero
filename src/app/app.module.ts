e1fc36c7c2badc50b8c0f462733c8692821759a6
RogerBariles <rogerbariles@gmail.com>
2020-05-19 13:14:48 -0300

    se inicializo la creacion de ps-web de cero

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './authentication/login/login.component';
+import { HeaderComponent } from './home/header/header.component';
+import { NavbarComponent } from './home/navbar/navbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    HeaderComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
